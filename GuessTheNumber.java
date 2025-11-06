4a18cc763fd12dce537a43a3a066ec649e91efe5
itsdroid <shubhampopalghat77@gmail.com>
2025-11-06 23:49:09 +0530

    updated file structure

---
+++ GuessTheNumber.java
@@ -0,0 +1,25 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = (int) Math.floor(Math.random() * 10);
+        int userNum;
+        do {
+            System.out.println("Guess the number : ");
+            userNum = sc.nextInt();
+
+            if (userNum == num) {
+                System.out.println("Wohoo!! , You guessed it right ");
+                break;
+            } else if (userNum < num) {
+                System.out.println("Guessed number is too low !");
+            } else {
+                System.out.println("Guessed number is too high !");
+            }
+        } while (userNum > 0);
+    }
+}
