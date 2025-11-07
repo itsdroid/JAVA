1bb08d617922c3270093e4352a0cf9e036d6d7a6
itsdroid <shubhampopalghat77@gmail.com>
2025-11-07 13:03:38 +0530

    updated SimpleCalculator

--- Practice/SimpleCalculator.java
+++ Practice/SimpleCalculator.java
@@ -1,42 +1,43 @@
+
 import java.util.Scanner;
+
 public class SimpleCalculator {
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("---------Simple Calculator------------");
-        System.out.println("Enter the first number: ");
+
+        System.out.println("-------------Welcome to simple calculator-------------");
+        System.out.print("Enter the first number: ");
         double num1 = sc.nextDouble();
 
-        System.out.println("Enter an operator: (+, -, *, /)");
+        System.out.print("Enter the operator (+, -, *, /): ");
         char operator = sc.next().charAt(0);
 
-        System.out.println("Enter the second number: ");
+        System.out.print("Enter the second number: ");
         double num2 = sc.nextDouble();
 
-        double result=0;
+        double result = 0;
+
         switch (operator) {
             case '+':
-            result = num1 + num2;
+                result = num1 + num2;
                 break;
             case '-':
-            result = num1 - num2;
+                result = num1 - num2;
                 break;
             case '*':
-            result = num1 * num2;
+                result = num1 * num2;
                 break;
             case '/':
-            if(num2 != 0) {
-            result = num1 / num2;
-            }
-            else {
-                System.out.println("Error division by zero");
-            }
-            break;
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error division by zero");
+                }
+                break;
             default:
-                System.out.print("Invalid input!");
+                System.out.println("Invalid input");
         }
-
-        System.out.print(result);
-
-
+        System.out.print("result: " + result);
     }
 }
