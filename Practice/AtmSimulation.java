8e05dd3d1497b0755df1da47d988f692401fee44
itsdroid <shubhampopalghat77@gmail.com>
2025-11-07 16:18:23 +0530

    updated AtmSimulation

---
+++ Practice/AtmSimulation.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class AtmSimulation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int balance = 50000;
+        int choice;
+
+        System.out.println("---- Welcome to ATM Simulation ----");
+
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Available Balance: " + balance);
+                    break;
+
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    int deposit = sc.nextInt();
+                    if (deposit > 0) {
+                        balance += deposit;
+                        System.out.println("Amount deposited successfully.");
+                    } else {
+                        System.out.println("Invalid deposit amount!");
+                    }
+                    System.out.println("Updated Balance: " + balance);
+                    break;
+
+                case 3:
+                    System.out.print("Enter amount to withdraw: ");
+                    int withdraw = sc.nextInt();
+                    if (withdraw > balance) {
+                        System.out.println("Insufficient balance!");
+                    } else if (withdraw <= 0) {
+                        System.out.println("Invalid withdrawal amount!");
+                    } else {
+                        balance -= withdraw;
+                        System.out.println("Withdrawal successful.");
+                    }
+                    System.out.println("Updated Balance: " + balance);
+                    break;
+
+                case 4:
+                    System.out.println("Thank you for using our ATM!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Try again!");
+            }
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
