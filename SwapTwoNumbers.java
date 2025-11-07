ea9b16bc4ebd25bfcfbaa851bba44566d5cae526
itsdroid <shubhampopalghat77@gmail.com>
2025-11-07 12:09:22 +0530

    updated user input

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -1,15 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class SwapTwoNumbers {
-
     public static void swap(int[] nums) {
         int temp = nums[0];
         nums[0] = nums[1];
         nums[1] = temp;
     }
+
     public static void main(String args[]) {
-        int[] nums = {5, 7};
+        Scanner sc = new Scanner(System.in);
+        // input two numbers
+        System.out.println("Enter the first number: ");
+        int[] nums = new int[2];
+        nums[0] = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        nums[1] = sc.nextInt();
+
         System.out.println("Before swap a: " + nums[0] + " and b: " + nums[1]);
         swap(nums);
-        System.out.println("After swap a: " + nums[0] + " and b: " + nums[1]);
+        System.out.println("Before swap a: " + nums[0] + " and b: " + nums[1]);
+
     }
 }
