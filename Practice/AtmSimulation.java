8acc9e8b9aeee0f5bad9d571e3ec55a93fbcd144
itsdroid <shubhampopalghat77@gmail.com>
2025-11-07 16:28:01 +0530

    updated pinCheck

--- Practice/AtmSimulation.java
+++ Practice/AtmSimulation.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 public class AtmSimulation {
+
+    public static boolean checkPincode(Scanner sc) {
+        int correctPin = 12345;
+        System.out.print("Enter your PIN: ");
+        int inputPin = sc.nextInt();
+
+        if (inputPin == correctPin) {
+            return true;
+        } else {
+            System.out.println("Incorrect PIN! Access denied");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int balance = 50000;
@@ -18,44 +32,45 @@ public class AtmSimulation {
 
             switch (choice) {
                 case 1:
-                    System.out.println("Available Balance: " + balance);
+                    if (checkPincode(sc)) {
+                        System.out.println("Available Balance: " + balance);
+                    }
                     break;
-
                 case 2:
-                    System.out.print("Enter amount to deposit: ");
-                    int deposit = sc.nextInt();
-                    if (deposit > 0) {
-                        balance += deposit;
-                        System.out.println("Amount deposited successfully.");
-                    } else {
-                        System.out.println("Invalid deposit amount!");
+                    if (checkPincode(sc)) {
+                        System.out.print("Enter amount to deposit: ");
+                        int deposit = sc.nextInt();
+                        if (deposit > 0) {
+                            balance += deposit;
+                            System.out.println("Amount deposited successfully.");
+                        } else {
+                            System.out.println("Invalid deposit amount!");
+                        }
+                        System.out.println("Updated Balance: " + balance);
                     }
-                    System.out.println("Updated Balance: " + balance);
                     break;
-
                 case 3:
-                    System.out.print("Enter amount to withdraw: ");
-                    int withdraw = sc.nextInt();
-                    if (withdraw > balance) {
-                        System.out.println("Insufficient balance!");
-                    } else if (withdraw <= 0) {
-                        System.out.println("Invalid withdrawal amount!");
-                    } else {
-                        balance -= withdraw;
-                        System.out.println("Withdrawal successful.");
+                    if (checkPincode(sc)) {
+                        System.out.print("Enter amount to withdraw: ");
+                        int withdraw = sc.nextInt();
+                        if (withdraw > balance) {
+                            System.out.println("Insufficient balance");
+                        } else if (withdraw <= 0) {
+                            System.out.println("Invalid withdrawal amount");
+                        } else {
+                            balance -= withdraw;
+                            System.out.println("Withdrawal successful");
+                        }
+                        System.out.println("Updated Balance: " + balance);
                     }
-                    System.out.println("Updated Balance: " + balance);
                     break;
-
                 case 4:
-                    System.out.println("Thank you for using our ATM!");
+                    System.out.println("Thank you for using our ATM");
                     break;
-
                 default:
-                    System.out.println("Invalid choice. Try again!");
+                    System.out.println("Invalid choice Try again!");
             }
         } while (choice != 4);
-
         sc.close();
     }
 }
